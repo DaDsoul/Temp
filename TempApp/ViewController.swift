9b8da5809bbbd884800e6d29b2820a047dc1643a
DaDsoul <ake.mussa@gmail.com>
2018-10-25 15:13:21 +0600

    Initial Commit

---
+++ TempApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempApp
+//
+//  Created by Akezhan Mussa on 10/25/18.
+//  Copyright © 2018 Akezhan Mussa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
